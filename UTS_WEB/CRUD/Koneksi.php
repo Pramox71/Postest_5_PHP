d1548ed410cb1e99751b5db09aaa0cba1004d7e8
Pramox71 <93047322+Pramox71@users.noreply.github.com>
2022-10-20 00:41:14 +0800

    Delete UTS_WEB directory

--- UTS_WEB/CRUD/Koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $conn = mysqli_connect("localhost", "root", "","website_postest");
-
-    if (!$conn) {
-        die("Gagal Terhubung dengan Data base" . mysqli_connect_error());
-    }
-
-?>
-
